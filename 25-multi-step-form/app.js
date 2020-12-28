a1b1d76c4db560dee983650bf5f54bc5ff07e1c2
belen-dominguez <belen.dominguez.sigler@outlook.com>
2020-12-28 14:17:53 -0300

    update multistep form

--- 25-multi-step-form/app.js
+++ 25-multi-step-form/app.js
@@ -1,4 +1,4 @@
-const totalCost = document.getElementById('plan-value');
+const totalCost = document.getElementById('plan-value'); 
 const btnValue = document.querySelectorAll('.btn');
 let radio = document.querySelectorAll('input[type="radio"]')
 const checkBox = document.querySelectorAll('input[type="checkbox"]')
@@ -7,6 +7,8 @@ const tableServices = document.querySelectorAll('.services-table');
 const tablePlans = document.querySelectorAll('.plan-table');
 const nxtBtn = document.querySelector('.nxtBtn');
 const prevBtn = document.querySelector('.prevBtn');
+const costDisplay = document.querySelector('.user-selection')
+const extraServices = [];
 
 let totalplanValue = 0;
 let planPrice  = 0;
@@ -15,11 +17,149 @@ let servicePrice  = 0;
 
 totalCost.innerText = totalplanValue;
 
+/*actualizar precios anual o menual */
+let percentOff = [];
+for(let i = 0; i < radio.length; i++){
+
+    percentOff.push((radio[i].value * 20) / 100);
+}
+let percentOffExtraSS = [];
+for(let i = 0; i < checkBox.length; i++){
+
+    percentOffExtraSS.push((checkBox[i].value * 20) / 100);
+}
+
+const btnMothPrice = document.querySelector('.btn-price-month');
+btnMothPrice.disabled = true
+
+const updatePrice = (optionSelected, btn) => {
+   /*estilos al btn seleccionado*/
+    const btnPriceChange = document.querySelectorAll('.btn-price-calc')
+    
+    btnPriceChange.forEach(item => {
+        item.classList.remove('selected')
+        item.disabled = false
+    })
+    btn.classList.add('selected')
+    btn.disabled = true
+   
+
+    const typeInput = [radio, checkBox];
+  
+    /*update valores de servicios*/
+    if(optionSelected == 'anual'){
+
+        typeInput.map(item => {
+        
+            for(let i = 0; i < item.length; i++){
+
+                /*cambio valor del valur*/
+                if(item[i].attributes.type.value == "radio" ){
+                    item[i].value = Number(item[i].value) - percentOff[i];
+                }
+                if(item[i].attributes.type.value == "checkbox"){
+                    item[i].value = Number(item[i].value) - percentOffExtraSS[i];
+                }
+                
+                /*cambio innertext del precio */
+                let str = item[i].previousElementSibling.innerText.split(' ')
+                str[1] = item[i].value;
+                let newStr = str.join(' ')
+    
+                item[i].previousElementSibling.dataset.value =  item[i].value
+                item[i].previousElementSibling.innerText = newStr
+            }
+
+
+        })
+
+    
+        // for(let i = 0; i < radio.length; i++){
+
+        //     /*cambio valor del valur*/
+        //     radio[i].value = Number(radio[i].value) - percentOff[i];
+            
+        //     /*cambio innertext del precio */
+        //     let str = radio[i].previousElementSibling.innerText.split(' ')
+        //     str[1] = radio[i].value;
+        //     let newStr = str.join(' ')
+
+        //     radio[i].previousElementSibling.dataset.value =  radio[i].value
+        //     radio[i].previousElementSibling.innerText = newStr
+        // }
+        // for(let i = 0; i < checkBox.length; i++){
+
+        //     /*cambio valor del valur*/
+        //     checkBox[i].value = Number(checkBox[i].value) - percentOffExtraSS[i];
+            
+        //     /*cambio innertext del precio */
+        //     let str = checkBox[i].previousElementSibling.innerText.split(' ')
+        //     str[1] = checkBox[i].value;
+        //     let newStr = str.join(' ')
+
+        //     checkBox[i].previousElementSibling.dataset.value =  checkBox[i].value
+        //     checkBox[i].previousElementSibling.innerText = newStr
+        // }
+
+    }
+    if(optionSelected == 'month'){
+
+        typeInput.map(item => {
+        
+            for(let i = 0; i < item.length; i++){
+  
+                /*cambio valor del valur*/
+                if(item[i].attributes.type.value == "radio" ){
+                   
+                    item[i].value = Number(item[i].value) + percentOff[i];
+                }
+                if(item[i].attributes.type.value == "checkbox"){
+                   
+                    item[i].value = Number(item[i].value) + percentOffExtraSS[i];
+                }
+                
+                /*cambio innertext del precio */
+                let str = item[i].previousElementSibling.innerText.split(' ')
+                str[1] = item[i].value;
+                let newStr = str.join(' ')
+    
+                item[i].previousElementSibling.dataset.value =  item[i].value
+                item[i].previousElementSibling.innerText = newStr
+            }
+            
+        })
+        
+    }
+    updateTotalCost(optionSelected)
+
+}
+
+/*actualziar percio al cambiar de menusal a anual y viceversa*/
+let priceUpdated;
+const updateTotalCost = (optionSelected) => {
+   
+    
+    let planSelected = document.querySelectorAll('.plan-container.selected')
+
+    planSelected = Array.from(planSelected)
+  
+    planSelected.forEach(item => {
+        //console.log(item.children[0].children[1].dataset.value)
+    })
+    priceUpdated = planSelected.reduce((acc,item) => {
+        return acc += Number(item.children[0].children[1].dataset.value)
+        
+    },0)
+   
+    totalCost.innerText = priceUpdated;
+}
 
 /*actualizar valor del plan*/
 const upgradeTotalCost = (isChecked, value, name, inputSelected) => {
 
     let tableContainer = inputSelected.parentElement.parentElement;
+    let extraServiceSelected = inputSelected.parentNode.children[0].innerHTML
+
    
     if(name == "plan"){
         planPrice =   Number(`${value}`);
@@ -30,27 +170,35 @@ const upgradeTotalCost = (isChecked, value, name, inputSelected) => {
         }
         tableContainer.classList.add('selected');
     }
-    console.log(planChoice)
     
     if(name == "servicio"){
         if(isChecked){
             servicePrice = servicePrice + Number(`${value}`);
             tableContainer.classList.add('selected');
 
-            inputSelected.labels[0].innerText = "Quitar"
+            inputSelected.labels[0].innerText = "Quitar";
+
+           /*agregar extra ss al array*/ 
+            extraServices.push(extraServiceSelected)
         }
         else {
             servicePrice = servicePrice - Number(`${value}`);
             tableContainer.classList.remove('selected');
 
             inputSelected.labels[0].innerText = "Seleccionar"
+
+            /*eliminar extra ss al array*/
+           let extraServiceOff = extraServices.indexOf(extraServiceSelected)
+           extraServices.splice(extraServiceOff, 1)
         }
     }
 
-    totalplanValue =  planPrice + servicePrice;
 
-    totalCost.innerText = totalplanValue;
+    // totalplanValue =  planPrice + servicePrice;
 
+    // totalCost.innerText = totalplanValue;
+    updateTotalCost()
+    
 }
 
 for(i=0; i < btnValue.length; i++){
@@ -61,7 +209,6 @@ for(i=0; i < btnValue.length; i++){
         let name = e.target.control.name;
         let isChecked = e.target.control.checked;
         let inputSelected = e.target.control
-
         upgradeTotalCost(isChecked, value, name, inputSelected)
     })
 
@@ -94,6 +241,8 @@ for(i=0; i < radio.length; i++){
 const nextStep = (value) => {
 
     prevBtn.style.display = "block";
+    nxtBtn.style.display = "block";
+    costDisplay.style.display = "flex";
     
     for(i=0; i < stepContainer.length; i++){
         stepContainer[i].classList.remove('active')
@@ -106,6 +255,7 @@ const nextStep = (value) => {
     }
     if(currentContainer == 2){
         nxtBtn.style.display = "none";
+        finalStep();
       
     }
  
@@ -114,8 +264,25 @@ const nextStep = (value) => {
 
 
 /*resumen */
-const selectedPlan = document.getElementById('selected-plan');
-const extraServices = document.getElementById('extra-services');
+const finalStep = () => {
+    const selectedPlan = document.getElementById('selected-plan');
+    const extraServicesTable = document.getElementById('extra-services');
+    const totalCostFinal = document.getElementById('plan-value-final');
+    
+    //totalCostFinal.innerHTML = totalplanValue; 
+    totalCostFinal.innerHTML = priceUpdated
+    selectedPlan.innerHTML = planChoice.toUpperCase();
+    costDisplay.style.display = "none";
 
-console.log(planChoice)
-selectedPlan.innerHTML = planChoice;
+    if(extraServices.length > 0){
+
+        extraServicesTable.innerHTML = extraServices.reduce((html, item) => {
+            return html + `
+                <li>${item}</li>
+            `
+        })
+    } else {
+        extraServicesTable.innerHTML = "No hay servicios adicionales seleccionados"
+    }
+
+}
