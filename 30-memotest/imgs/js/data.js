cbe6f20064afb54048ba792bdc67f04bc66390c1
belen-dominguez <belen.dominguez.sigler@outlook.com>
2021-01-30 22:09:08 -0300

    memotest mobile

---
+++ 30-memotest/imgs/js/data.js
@@ -0,0 +1,42 @@
+const images = [
+    {
+       img:  "./imgs/buho.jpg",
+       name: 'buho'
+    },
+    {
+        img:  "./imgs/conejo.jpg",
+        name: 'conejo'
+    },
+    {
+        img:  "./imgs/elefante.jpg",
+        name: 'elefante'
+    },
+    {
+        img:  "./imgs/gallina.jpg",
+        name: 'gallina'
+    },
+    {
+        img:  "./imgs/gato-rosa.jpg",
+        name: 'gato-rosa'
+    },
+    {
+        img:    "./imgs/jirafa.jpg",
+        name: 'jirafa'
+    },
+    {
+        img:   "./imgs/leon.jpg",
+        name: 'leon'
+    },
+    {
+        img:  "./imgs/mono.jpg",
+        name: 'mono'
+    },
+    {
+        img:  "./imgs/oso-blanco.jpg",
+        name: 'oso-blanco'
+    },
+    {
+        img:   "./imgs/reno.jpg",
+        name: 'reno'
+    }
+]
